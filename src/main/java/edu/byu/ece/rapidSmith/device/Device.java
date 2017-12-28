ae9b52249661f99de8ed4b5bc322846dbe1a2cd3
trharoldsen <trharoldsen@gmail.com>
2017-12-27 22:20:45 -0500

    Fixed missing value

--- src/main/java/edu/byu/ece/rapidSmith/device/Device.java
+++ src/main/java/edu/byu/ece/rapidSmith/device/Device.java
@@ -726,6 +726,7 @@ public class Device implements Serializable {
 		private Collection<SiteTemplate> siteTemplates;
 		private PrimitiveDefList primitiveDefs;
 		private Map<String, PackagePin> packagePinMap;
+		private Integer numUniqueWireTypes;
 
 		public void readResolve(Device device) {
 			device.partName = partName;
@@ -746,6 +747,7 @@ public class Device implements Serializable {
 				device.siteTemplates.put(template.getType(), template);
 			}
 			device.primitiveDefs = primitiveDefs;
+			device.numUniqueWireTypes = (numUniqueWireTypes != null) ? numUniqueWireTypes : -1;
 
 			device.constructTileMap();
 			device.constructDependentResources();
@@ -778,5 +780,6 @@ public class Device implements Serializable {
 		repl.siteTemplates = siteTemplates.values();
 		repl.primitiveDefs = primitiveDefs;
 		repl.packagePinMap = packagePinMap;
+		repl.numUniqueWireTypes = numUniqueWireTypes;
 	}
 }
